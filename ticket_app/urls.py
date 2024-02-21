94cdb3e81499b01e3be3d6c92e63f3cd149e551c
Harvey Martin <harvey.martin@bp.com>
2024-02-21 14:45:27 +0000

    Added landing page

--- ticket_app/urls.py
+++ ticket_app/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('',include('users.urls', namespace='users')),
     path('admin/', admin.site.urls),
     path('tickets/', include('tickets.urls', namespace='tickets')), 
 ]
