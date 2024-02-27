d2a6f70174f103dd51e75f36876193e978a1a603
Harvey Martin <harvey.martin@bp.com>
2024-02-27 12:13:23 +0000

    Added one logout button

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'users'
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('', views.landing_page, name='landing_page'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
